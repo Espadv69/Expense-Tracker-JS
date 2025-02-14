941e978cf558fecb2faa1cbf2f06f77f7f30acea
Espadv69 <aitorjob69@gmail.com>
2025-02-14 12:30:04 +0100

    Add DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // DOM elements
+  const $expenseName = document.querySelector('.expenseName')
+  const $expenseAmount = document.querySelector('.expenseAmount')
+  const $addExpenseBtn = document.querySelector('.addExpense')
+  const $expenseList = document.querySelector('.expenseList')
+  const $totalAmount = document.querySelector('.totalAmount')
+})
