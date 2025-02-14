1588fae2c2154815f9c7bd2d8abc5c676386adf2
Espadv69 <aitorjob69@gmail.com>
2025-02-14 12:36:43 +0100

    Feat: Add function to add expense

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const $addExpenseBtn = document.querySelector('.addExpense')
   const $expenseList = document.querySelector('.expenseList')
   const $totalAmount = document.querySelector('.totalAmount')
+
+  $addExpenseBtn.addEventListener('click', addExpense) // toDo
+
+  function addExpense() {
+    const name = $expenseName.value.trim()
+    const amount = parseFloat($expenseAmount.value)
+
+    if (!name || isNaN(amount) || amount <= 0) return
+
+    const expense = { name, amount }
+    saveExpense(expense) // toDo
+    renderExpense(expense) // toDo
+    updateTotal() // toDo
+
+    $expenseName.value = ''
+    $expenseAmount.value = ''
+  }
 })
